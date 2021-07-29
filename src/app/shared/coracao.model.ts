e9b895a95321301af56f7fe5717d6bdb799910ad
FabioFS <fabiofigueiredo.net@gmail.com>
2021-07-29 00:28:52 -0300

    update

--- src/app/shared/coracao.model.ts
+++ src/app/shared/coracao.model.ts
@@ -3,8 +3,8 @@ export class Coracao {
 
     constructor(
         public cheio: boolean,
-        public urlCoracaoCheio: string = '/assets/coracao_cheio.png',
-        public urlCoracaoVazio: string = '/assets/coracao_vazio.png'
+        public urlCoracaoCheio: string = 'assets/coracao_cheio.png',
+        public urlCoracaoVazio: string = 'assets/coracao_vazio.png'
         ){}
 
     public exibeCoracao(): string{
