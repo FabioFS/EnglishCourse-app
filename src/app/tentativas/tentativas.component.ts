4b4ab2443e7d679440d1bcc39e36a9214a00800f
FabioFS <fabiofigueiredo.net@gmail.com>
2021-07-28 22:01:51 -0300

    Update App!

--- src/app/tentativas/tentativas.component.ts
+++ src/app/tentativas/tentativas.component.ts
@@ -1,18 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import { Coracao } from './../shared/coracao.model';
+import { Component, OnInit, Input, OnChanges } from '@angular/core';
+
 
 @Component({
   selector: 'app-tentativas',
   templateUrl: './tentativas.component.html',
   styleUrls: ['./tentativas.component.css']
 })
-export class TentativasComponent implements OnInit {
+export class TentativasComponent implements OnChanges {
 
-  public coracaoVazio: string = '../assets/coracao_vazio.png'
-  public coracaoCheio: string = '../assets/coracao_cheio.png'
+  public coracoes: Coracao[] = [
+    new Coracao(true),
+    new Coracao(true),
+    new Coracao(true),
+  ]
 
-  constructor() { }
+  @Input() public tentativas: number = 0 
+
+  constructor() { console.log(this.coracoes)}
 
   ngOnInit(): void {
+   
+  }
+  
+  ngOnChanges(): void {
+
+    if(this.tentativas !== this.coracoes.length){
+
+      let indice =  this.coracoes.length - this.tentativas
+
+      this.coracoes[indice - 1].cheio = false
+    }
+    
+
+    console.log("tentativas: ", this.tentativas)
   }
 
 }
